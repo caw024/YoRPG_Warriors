77f36065bc52ef39994443876ca3d756897f5247
YingXin7 <yjiang2@stuy.edu>
2017-11-13 16:05:13 -0500

    commit

--- v3/Character.java
+++ v3/Character.java
@@ -1,29 +1,26 @@
-public class Character{
+public class Character {
     
     public static int life;
     public static int strength;
     public static int defense;
     public static double aRate;
 
-    public static boolean isAlive(){
+    public static boolean isAlive() {
 	if (life > 0)
 	    return true;
 	return false;
     }
 
-    public static int getDefense(){
+    public static int getDefense() {
 	return defense;
     }
 
-    public static int lowerHP(int n){
+    public static int lowerHP(int n) {
 	return life -= n;
     }
 
-    public static int attack(Character n){
+    public static int attack(Character n) java{
 	int damage = (int) (strength * aRate) - n.getDefense();
 	return damage;
-    }
-    
-    
-  
+    }  
 }
